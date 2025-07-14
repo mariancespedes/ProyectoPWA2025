d652e2d156940914ebc412cf09223b938bf270bd
Mariano Céspedes <cespedesmj.contacto@gmail.com>
2025-07-14 03:16:17 -0300

    Agregamos la vista, reseña

--- ProyectoPWA2025/Controllers/HomeController.cs
+++ ProyectoPWA2025/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace ProyectoPWA2025.Controllers
         {
             return View();
         }
+        public IActionResult Reseña() 
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
